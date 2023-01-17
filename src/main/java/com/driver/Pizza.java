dd5b30dd9993559b7284caf517f4eeaf10d48417
Abilash S L <abilash0045@gmail.com>
2023-01-17 18:13:04 +0530

    soln1

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -6,9 +6,20 @@ public class Pizza {
     private Boolean isVeg;
     private String bill;
 
+    boolean cheese = false;
+    boolean topings = false;
+    boolean takeaway = false;
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if (isVeg){
+            price = 300;
+            bill = "300";
+        }else{
+            price = 400;
+            bill = "400";
+        }
+        System.out.println("Base Price Of The Pizza: " + price);
     }
 
     public int getPrice(){
@@ -17,18 +28,39 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if (cheese == false){
+            System.out.println("Extra Cheese Added: 80");
+            bill = String.valueOf(Integer.parseInt(bill) + 80);
+            cheese = true;
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if (topings == false && isVeg){
+            System.out.println("Extra Toppings Added: 70");
+            bill = String.valueOf(Integer.parseInt(bill) + 70);
+            topings = true;
+        }
+        else if (topings == false && !isVeg){
+            System.out.println("Extra Toppings Added: 120");
+            bill = String.valueOf(Integer.parseInt(bill) + 120);
+            topings = true;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        if (takeaway == false) {
+            System.out.println("Paperbag Added: 20");
+            bill = String.valueOf(Integer.parseInt(bill) + 20);
+            takeaway = true;
+        }
     }
 
     public String getBill(){
         // your code goes here
+        System.out.print("Total Price: ");
         return this.bill;
     }
 }
